c05946b952537ff331c5315d57c2c7d1456f5a3f
chasethewhiterabbitt <trmchale@gmail.com>
2013-12-08 20:55:15 +0000

    Updates to Assignment 6

--- Assignment6prototype.cpp
+++ Assignment6prototype.cpp
@@ -1,12 +1,11 @@
 // Timothy McHale Assignment 6
 
 // So this is a simple program that reads 10 
-// dates that will be stored by string values in our program
-// Our main program won't do any of the heavy lifting
-// it will mainly call other functions
-// The first function called will be readoriginaldate
-// readoriginaldate will be a void function, 
-// and will store one reference parameter date 
+// dates that will be stored by string values 
+// The main program will initialize four variables and call three functions
+// The first of these functions will be 
+// readoriginaldate, will  be a void function
+// it will store one reference parameter date, 
 // date will be used by all our other functions in the program.
 // The second function we will use in the program is breakoriginaldate
 // breakoriginal date will have four parameters, the first will the date read in by readoriginaldate
@@ -66,12 +65,13 @@ int main ()
 	// and stores it in a reference parameter for use later in the program
 	void readoriginaldate(string &date)
 {
-	outfile << "Enter the date ..." << endl;
 	outfile << endl;
 	infile >> date;
 	outfile << endl;
 	outfile << "The date you entered was " << date << endl;
 }
+
+
 	// breakoriginaldate has four parameters
 	// The first is a string value date and we break that into three
 	// seperate string values that is stored 
@@ -128,7 +128,10 @@ int main ()
 	
 	outfile << endl;
 }
-	// print date takes three parameters.
+	
+
+
+        // printdate3ways takes three parameters.
 	// month, day, year
 	// it prints these strings in three seperate ways 
 	// which will be seen in the output
@@ -137,7 +140,6 @@ int main ()
 	// The American way was a little more difficult
 	// I assigned the month ex. 01 a string called january
 	// then I printed the date using that variable instead of the word "January "
-	
 	// Admittedly if there is a more efficient algorithm, point me in that direction for th sake of future programs
 	
 	void printdate3ways (string month, string day, string year)
@@ -148,77 +150,79 @@ int main ()
 	
 	outfile << "The European way ... "<< print1 << endl;
 	
-	january = "January ";
-	febuary = "Febuary ";
-	march = "March " ;
-	april = "April ";
-	may = "May ";
-	june = "June ";
-	july = "July ";
-	august = "August ";
-	september = "September ";
-	october = "October ";
-	november = "November ";
-	december = "December ";
+	january = "January " + day + "," + " 20" + year;
+	febuary = "Febuary " + day + "," + " 20" + year;
+	march = "March " + day + "," + " 20" + year;
+	april = "April " + day + "," + " 20" + year;
+	may = "May " + day + "," + " 20" + year;
+	june = "June " + day + "," + " 20" + year;
+	july = "July " + day + "," + " 20" + year;
+	august = "August " + day + "," + " 20" + year;
+	september = "September " + day + "," + " 20" + year;
+	october = "October " + day + "," + " 20" + year;
+	november = "November " + day + "," + " 20" + year;
+	december = "December " + day + "," + " 20" + year; 
 	
-	if (month == "01")
+	
+
+        if  (month == "01")
 	{
-		outfile << january + day + "," + " 20" + year << endl;
+		outfile << january << endl;
 	}
 	
 	if (month == "02")
 	{
-		outfile << febuary + day +  "," + " 20" + year << endl;
+		outfile << febuary << endl;
 	}
 	
 	if (month == "03")
 	{
-		outfile << march + day + "," + " 20" + year << endl;
+		outfile << march << endl;
 	}
 	
 	if (month == "04")
 	{
-		outfile << april + day + "," + " 20" + year << endl;
+		outfile << april << endl;
 	}
 	
 	if (month == "05")
 	{
-		outfile << may + day + "," + " 20" + year << endl;
+		outfile << may << endl;
 	}
 	
 	if (month == "06")
 	{
-		outfile << june + day + "," + " 20" + year << endl;
+		outfile << june << endl;
 	}
 	
 	if (month == "07")
 	{
-		outfile << july + day + "," + " 20" + year << endl;
+		outfile << july << endl;
 	}
 	
 	if (month == "08")
 	{
-		outfile << august + day + "," + " 20" + year << endl;
+		outfile << august << endl;
 	}
 	
 	if (month == "09")
 	{
-		outfile << september + day + "," + " 20" + year << endl;
+		outfile << september << endl;
 	}
 	
 	if (month == "10")
 	{
-		outfile << october + day + "," + " 20" + year << endl;
+		outfile << october << endl;
 	}
 	
 	if (month == "11")
 	{
-		outfile << november + day + "," + " 20" + year << endl;
+	  outfile << november << endl;
 	}
 	
 	if (month == "12")
 	{
-		outfile << december + day + "," + " 20" + year << endl;
+		outfile << december << endl;
 	}	
 	
 	print2 = month + "-" + day + "-" + "20" + year;
